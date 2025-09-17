eca9d4cfcde27334bf5dfcc261cc390dd1daef8c
Ali Amiri <ali@across.ai>
2025-09-16 20:24:02 -0700

    fix and update

--- setup_mac.sh
+++ setup_mac.sh
@@ -18,7 +18,7 @@ fi
 if ! command_exists brew ; then
   echo "Homebrew not found. Installing Homebrew..."
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-  echo >> "${HOME}/.zprofile
+  echo >> "${HOME}/.zprofile"
   echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> "${HOME}/.zprofile"
   eval "$(/opt/homebrew/bin/brew shellenv)"
 else
@@ -32,7 +32,7 @@ brew update
 # Install Homebrew Packages
 echo "Installing Homebrew packages..."
 brew install git openssh emacs gh jq
-brew install python@3.12 gcc@13
+brew install python@3.13 gcc@14
 brew install protobuf
 
 brew install tailscale
@@ -41,7 +41,7 @@ brew install tailscale
 
 # Install Go
 echo "Installing Go..."
-GO_VERSION="1.23.2"
+GO_VERSION="1.25.1"
 ARCH=$(uname -m)
 
 if [ "$ARCH" = "arm64" ]; then
@@ -56,15 +56,15 @@ sudo rm -rf /usr/local/go
 sudo tar -C /usr/local -xzf go${GO_VERSION}.darwin-${GO_ARCH}.tar.gz
 rm go${GO_VERSION}.darwin-${GO_ARCH}.tar.gz
 
-echo 'export PATH=$PATH:/usr/local/go/bin' >> ~/.zprofiles
+echo 'export PATH=$PATH:/usr/local/go/bin' >> ~/.zprofile
 
 brew install --cask google-chrome
 brew install --cask rectangle   # https://rectangleapp.com/
 brew install --cask visual-studio-code
 # Uncomment if needed.
 # sudo ln -sf "/Applications/Visual Studio Code.app/Contents/Resources/app/bin/code" /usr/local/bin/code
-code --install-extension ms-python.python
-code --install-extension golang.go
+code --install-extension --force ms-python.python
+code --install-extension --force golang.go
 
 echo "Setup complete! Please restart your terminal or run 'source ~/.zprofile' to apply the changes."
 
@@ -74,17 +74,25 @@ brew tap hashicorp/tap
 brew install hashicorp/tap/terraform
 
 # install gcloud cli
-curl -OL https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-cli-darwin-arm.tar.gz
+if [ "$ARCH" = "arm64" ]; then
+    GCLOUD_ARCH="arm"
+else
+    GCLOUD_ARCH="x86_64"
+fi
+
+curl -OL https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-cli-darwin-${GCLOUD_ARCH}.tar.gz
+mkdir -p $HOME/local/bin
 rm -rf $HOME/local/bin/google-cloud-sdk
-tar xvf google-cloud-cli-darwin-arm.tar.gz -C $HOME/local/bin 
+tar xvf google-cloud-cli-darwin-${GCLOUD_ARCH}.tar.gz -C $HOME/local/bin
 $HOME/local/bin/google-cloud-sdk/install.sh
-rm -rf google-cloud-cli-darwin-arm.tar.gz
+rm -rf google-cloud-cli-darwin-${GCLOUD_ARCH}.tar.gz
 
 gcloud components install gke-gcloud-auth-plugin
 
 # install podman and kubectl
 # TODO(iampat): Use the recommended method https://podman.io/docs/installation
 brew install podman
+brew install --cask podman-desktop
 brew install kubectl
 brew install helm
 
@@ -94,3 +102,9 @@ npm install -g npm@latest
 
 # install mermaid-cli
 npm install -g @mermaid-js/mermaid-cli
+
+# install Claude CLI
+npm install -g @anthropic-ai/claude-code
+
+# install Gemini CLI
+npm install -g @google/gemini-cli@latest
